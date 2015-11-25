2acb9d29eaa4b7b1b497a8f2dd6c394c228ac856
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2015-11-25 12:17:12 -0600

    Email Send File
    Proyecto para enviar archivo de excel a través de SMTP.

---
+++ src/sendattachmentinemail/SendAttachmentInEmail.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sendattachmentinemail;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class SendAttachmentInEmail {
+   public static void main(String[] args) {
+      // Recipient's email ID needs to be mentioned.
+      String to = "direccion@veotek.mx";
+
+      // Sender's email ID needs to be mentioned
+      String from = "gerencia@veotek.mx";
+
+      final String username = "gerencia@veotek.mx";//change accordingly
+      final String password = "uV0CvCwR5#I[";//change accordingly
+
+      // Assuming you are sending email through relay.jangosmtp.net
+      String host = "mail.veotek.mx";
+
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "26");
+
+      // Get the Session object.
+      Session session = Session.getInstance(props,
+         new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+               return new PasswordAuthentication(username, password);
+            }
+         });
+
+      try {
+         // Create a default MimeMessage object.
+         Message message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.setRecipients(Message.RecipientType.TO,
+            InternetAddress.parse(to));
+
+         // Set Subject: header field
+         message.setSubject("Testing Subject");
+
+         // Create the message part
+         BodyPart messageBodyPart = new MimeBodyPart();
+
+         // Now set the actual message
+         messageBodyPart.setText("Estado de Cuenta Veotek");
+
+         // Create a multipar message
+         Multipart multipart = new MimeMultipart();
+
+         // Set text message part
+         multipart.addBodyPart(messageBodyPart);
+
+         // Part two is attachment
+         messageBodyPart = new MimeBodyPart();
+         String filename = "Estado-Cuenta-Veotek.xlsx";
+         DataSource source = new FileDataSource(filename);
+         messageBodyPart.setDataHandler(new DataHandler(source));
+         messageBodyPart.setFileName(filename);
+         multipart.addBodyPart(messageBodyPart);
+
+         // Send the complete message parts
+         message.setContent(multipart);
+
+         // Send message
+         Transport.send(message);
+
+         System.out.println("Sent message successfully....");
+  
+      } catch (MessagingException e) {
+         throw new RuntimeException(e);
+      }
+   }
+}
